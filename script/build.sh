09522ca7cfe83325088ed35e8b198f98492203fe
Akihiro TAKASE <42milez@gmail.com>
2020-08-04 21:02:33 +0900

    clean up

--- script/build.sh
+++ script/build.sh
@@ -5,11 +5,11 @@ source lib/bash-utils/main.sh "$(pwd)/lib/bash-utils"
 
 cd $BUILD_DIR_PATH || exit
 
-$CMAKE .. \
+cmake .. \
   -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
   -DCMAKE_MAKE_PROGRAM=/usr/bin/make \
   -DCMAKE_C_COMPILER=$CC \
   -DCMAKE_CXX_COMPILER=$CXX \
   -G 'CodeBlocks - Unix Makefiles'
 
-$CMAKE --build . --target all -- -j 8
+cmake --build . --target all -- -j 8
--- script/test.sh
+++ script/test.sh
@@ -5,4 +5,4 @@ source lib/bash-utils/main.sh "$(pwd)/lib/bash-utils"
 
 cd $BUILD_DIR_PATH || exit
 
-$CTEST --output-on-failure
+ctest --output-on-failure
