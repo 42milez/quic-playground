6f7c173baefdd94292ebd9b250217e96197de4f4
Akihiro TAKASE <42milez@gmail.com>
2020-08-13 14:12:35 +0900

    update

--- vagrant/transfer.sh
+++ vagrant/transfer.sh
@@ -41,12 +41,36 @@ fi
 
 cat - << 'EOS' >> "$CHROMIUM_DIR/CMakeLists.txt"
 set_source_files_properties(
+    base/task/thread_pool/job_task_source.cc
+    PROPERTIES COMPILE_FLAGS -Wno-c++11-narrowing
+)
+set_source_files_properties(
+    base/task/current_thread.cc
+    base/task/sequence_manager/real_time_domain.cc
+    base/task/sequence_manager/sequence_manager.cc
+    base/task/sequence_manager/sequence_manager_impl.cc
+    base/task/sequence_manager/task_queue.cc
+    base/task/sequence_manager/task_queue_impl.cc
+    base/task/sequence_manager/task_queue_selector.cc
+    base/task/sequence_manager/thread_controller_impl.cc
+    base/task/sequence_manager/thread_controller_with_message_pump_impl.cc
+    base/task/sequence_manager/time_domain.cc
+    base/task/sequence_manager/work_queue.cc
+    base/task/sequence_manager/work_queue_sets.cc
+    base/threading/thread.cc
+    PROPERTIES COMPILE_FLAGS -Wno-final-dtor-non-final-class
+)
+set_source_files_properties(
     base/json/json_writer.cc
     PROPERTIES COMPILE_FLAGS -Wno-implicit-int-float-conversion
 )
 set_source_files_properties(
     base/time/time.cc
     PROPERTIES COMPILE_FLAGS -Wno-narrowing
+)
+set_source_files_properties(
+    third_party/tcmalloc/chromium/src/heap-checker.cc
+    PROPERTIES COMPILE_FLAGS -Wno-string-compare
 )
 set_source_files_properties(
     base/check.cc
@@ -57,20 +81,19 @@ set_source_files_properties(
     PROPERTIES COMPILE_FLAGS -Wno-unknown-pragmas
 )
 set_source_files_properties(
-    base/task/sequence_manager/task_queue_impl.h
-    PROPERTIES COMPILE_FLAGS -Wno-final-dtor-non-final-class
+    third_party/tcmalloc/chromium/src/elf_mem_image.cc
+    third_party/tcmalloc/chromium/src/system-alloc.cc
+    PROPERTIES COMPILE_FLAGS -Wno-unused-const-variable
 )
 set_source_files_properties(
-    base/task/thread_pool/job_task_source.cc
-    PROPERTIES COMPILE_FLAGS -Wno-c++11-narrowing
-)
-set_source_files_properties(
+    third_party/tcmalloc/chromium/src/free_list.cc
     third_party/tcmalloc/chromium/src/heap-profiler.cc
+    third_party/tcmalloc/chromium/src/system-alloc.cc
     PROPERTIES COMPILE_FLAGS -Wno-unused-function
 )
 set_source_files_properties(
-    third_party/tcmalloc/chromium/src/heap-checker.cc
-    PROPERTIES COMPILE_FLAGS -Wno-string-compare
+    third_party/tcmalloc/chromium/src/system-alloc.cc
+    PROPERTIES COMPILE_FLAGS -Wno-unused-variable
 )
 EOS
 
